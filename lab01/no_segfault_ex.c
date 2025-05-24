47f1326223f19adc8070517c1ba5fca28c339fa9
MotingLiuu <moting.liuu@gmail.com>
2025-05-24 11:44:22 +0900

    update lab01

--- lab01/no_segfault_ex.c
+++ lab01/no_segfault_ex.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main() {
-    int a[5] = {1, 2, 3, 4, 5};
-    unsigned total = 0;
-    for (int j = 0; j < sizeof(a); j++) {
+    int a[5] = {1, 2, 3, 4, 5}; // int array
+    unsigned total = 0; 
+    for (int j = 0; j < sizeof(a); j++) { // sizeof(a) may depends on the system? a is a pointer?
         total += a[j];
     }
     printf("sum of array is %d\n", total);
