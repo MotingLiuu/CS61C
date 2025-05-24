4b7792f483f63bc7de31034a849f329c18167a88
MotingLiuu <moting.liuu@gmail.com>
2025-05-24 12:38:35 +0900

    done lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,23 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *p1 = head;
+    node *p2 = head;
+    while (p1 != NULL) {
+        p1 = p1 -> next;
+        if (p1 == NULL) {
+            break;
+        } else {
+            p1 = p1 -> next;
+        }
+        if (p1 == NULL) {
+            break;
+        } else {
+            p2 = p2 -> next;
+            if (p1 == p2) {
+                return 1;
+            }
+        }
+    }
     return 0;
 }
--- lab01/no_segfault_ex.c
+++ lab01/no_segfault_ex.c
@@ -2,8 +2,9 @@
 int main() {
     int a[5] = {1, 2, 3, 4, 5}; // int array
     unsigned total = 0; 
-    for (int j = 0; j < sizeof(a); j++) { // sizeof(a) may depends on the system? a is a pointer?
+    for (int j = 0; j < sizeof(a); j++) { // a is a real array, not a pointer. so sizeof(a) is 20.
         total += a[j];
     }
+    printf("size of a is %d\n", sizeof(a));
     printf("sum of array is %d\n", total);
 }
